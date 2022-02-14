0a0d631395a25793a1b81b5f54d15aedc3f34555
Valeriy <manikala777@rambler.ru>
2022-02-14 22:00:53 +0300

    the end lesson 8

--- springtest/src/main/java/spring/app1/ClassicalMusic.java
+++ springtest/src/main/java/spring/app1/ClassicalMusic.java
@@ -1,6 +1,21 @@
 package spring.app1;
 
 public class ClassicalMusic implements Music {
+
+    private ClassicalMusic () {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Doimg my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Bethoven";
--- springtest/src/main/java/spring/app1/TestString.java
+++ springtest/src/main/java/spring/app1/TestString.java
@@ -7,10 +7,15 @@ public class TestString {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
 
         //MusicPlayer musicPlayer = new MusicPlayer (music);
 
+/*
         MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         //musicPlayer.playMusic();
@@ -23,6 +28,7 @@ public class TestString {
         firstMusicPlayer.setVolume(10);
         System.out.println(firstMusicPlayer.getVolume());
         System.out.println(secondMusicPlayer.getVolume());
+*/
 
         //System.out.println(musicPlayer.getName());
         //System.out.println(musicPlayer.getVolume());
