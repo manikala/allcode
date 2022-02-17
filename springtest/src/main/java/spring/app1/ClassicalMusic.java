614d669a9c5233ad7d6ff166a1004a4b2d9f90b8
Valeriy <manikala777@rambler.ru>
2022-02-17 22:10:19 +0300

    the end lesson 12 не рабочая инициализация

--- springtest/src/main/java/spring/app1/ClassicalMusic.java
+++ springtest/src/main/java/spring/app1/ClassicalMusic.java
@@ -1,9 +1,21 @@
 package spring.app1;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Start to initiliziasion");
+    }
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
 
     private ClassicalMusic () {}
 
